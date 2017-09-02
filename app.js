34b0d80b9b5438d9fda37aa97699a0a1af86bc6a
seansio1995 <seansio@163.com>
2017-09-02 18:26:10 -0400

    add lesson 9

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el:"#vue-app",
   data:{
     name:"",
-    age:""
+    age:20,
+    a:0,
+    b:0,
   },
   methods:{
       logName:function(){
@@ -11,6 +13,31 @@ new Vue({
 
       logAge:function(){
         console.log("You enter your age");
-      }
+      },
+
+
+    // addAgeA:function(){
+    //   console.log("Add A");
+    //   return this.age+this.a;
+    // },
+    //
+    //
+    // addAgeB:function(){
+    //   console.log("Add B");
+    //   return this.age+this.b;
+    // }
+  },
+
+  computed:{
+    addAgeA:function(){
+      console.log("Add A");
+      return this.age+this.a;
+    },
+
+
+    addAgeB:function(){
+      console.log("Add B");
+      return this.age+this.b;
     }
+  }
 });
