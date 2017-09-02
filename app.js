22a9a438302a448b15e3a0acf839a84ab5389947
seansio1995 <seansio@163.com>
2017-09-02 15:02:12 -0400

    add lesson5

--- app.js
+++ app.js
@@ -4,12 +4,35 @@ new Vue({
     name:"Sean Xiao",
     job:"Student",
     website:"http://www.thenetninja.co.uk",
-    websiteTag:'<a href="http://www.thenetninja.co.uk">Net Ninja</a>'
+    websiteTag:'<a href="http://www.thenetninja.co.uk">Net Ninja</a>',
+    age:22,
+    x:0,
+    y:0,
   },
   methods:{
     greet: function(time){
       return "Good "+time+" "+this.name;
+    },
+    add: function(){
+      this.age++;
+    },
+
+    subtract:function(){
+      this.age--;
+    },
+
+    addten:function(){
+      this.age += 10;
+    },
+
+    subtractten:function(){
+      this.age-=10;
+    },
+
+    updateXY:function(event){
+      this.x=event.offsetX;
+      this.y=event.offsetY;
     }
 
-  }
+}
 });
