7b0458b4612cf895a16f9069901807c15218902a
seansio1995 <seansio@163.com>
2017-09-02 18:09:12 -0400

    add lesson 7

--- app.js
+++ app.js
@@ -1,42 +1,15 @@
 new Vue({
   el:"#vue-app",
   data:{
-    name:"Sean Xiao",
-    job:"Student",
-    website:"http://www.thenetninja.co.uk",
-    websiteTag:'<a href="http://www.thenetninja.co.uk">Net Ninja</a>',
-    age:22,
-    x:0,
-    y:0,
+
   },
   methods:{
-    greet: function(time){
-      return "Good "+time+" "+this.name;
-    },
-    add: function(){
-      this.age++;
-    },
+      logName:function(){
+        console.log("You enter your name");
+      },
 
-    subtract:function(){
-      this.age--;
-    },
-
-    addten:function(){
-      this.age += 10;
-    },
-
-    subtractten:function(){
-      this.age-=10;
-    },
-
-    updateXY:function(event){
-      this.x=event.offsetX;
-      this.y=event.offsetY;
-    },
-
-    click:function(){
-      alert("You clicked me once!");
+      logAge:function(){
+        console.log("You enter your age");
+      }
     }
-
-}
 });
